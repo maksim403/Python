1c47708acf7beadae0496eddb8d93ad9609ba1b3
maxim403 <poryaginmaxim@yandex.ru>
2025-08-31 14:30:28 +0300

    lesson_1

--- lesson1/lesson_1.py
+++ lesson1/lesson_1.py
@@ -5,28 +5,27 @@ my_age = 48
 my_age = my_age + 3
 print(my_age)
 
-first_name = input("Ваше имя: Maksim")
-last_name = input("Ваша фамилия: Poryagin")
-print("Вас зовут:" + last_name + first_name)
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print(f"Вас зовут, {first_name} {last_name}")
 
-def print_greeting(name) :
-    print("Привет," + name)
-    print_greeting("мир!")
+def print_greeting() :
+    print("Привет, мир!")
+print_greeting()
 
-def num():
-    print(8)
-    print(8)
-    print(0)
-    print(0)
-    print(5)
-    print(5)
-    print(5)
-    print(3)
-    print(5)
-    print(3)
-    print(5)
-
-num()
+def print_num(num):
+    print(num, end='')
+print_num(8)
+print_num(8)
+print_num(0)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
 
 
 
