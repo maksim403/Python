349dab793b090b67ebb9c287cc76ba8b49b01a0e
maxim403 <poryaginmaxim@yandex.ru>
2025-09-09 06:53:26 +0300

    lesson_3

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -8,4 +8,13 @@ from_address = Address("8745", "Орск", "Высокая", "23", "45")
 
 mailing = Mailing(to_address, from_address, 300, "trk67854")
 
-print(mailing)
+print(
+    f"Отправление {mailing.track} из {mailing.to_address.index}, "
+    f"{mailing.to_address.city}, "
+    f"{mailing.from_address.street}, {mailing.from_address.house} - "
+    f"{mailing.from_address.apartment} в {mailing.from_address.index}, "
+    f"{mailing.from_address.city}, "
+    f"{mailing.to_address.street}, {mailing.to_address.house} - "
+    f"{mailing.to_address.apartment}. "
+    f"Стоимость {mailing.cost} рублей."
+)
